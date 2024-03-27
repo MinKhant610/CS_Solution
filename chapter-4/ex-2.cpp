0a1b156fddba7e18b2a5c24edc479e954ba6cf82
MinKhant610 <minkhantiopp@gmail.com>
2024-03-27 16:07:08 +0630

    update 4.2

--- chapter-4/ex-2.cpp
+++ chapter-4/ex-2.cpp
@@ -4,15 +4,16 @@ using namespace std;
 struct Point{
     int x;
     int y;
+    int z;
 };
 
 int main(){
     Point p1, p2;
     cout << "Enter coordinates for p1 : ";
-    cin >> p1.x >> p1.y;
+    cin >> p1.x >> p1.y >> p1.z;
     cout << "Enter coordinates for p2 : ";
-    cin >> p2.x >> p2.y;
-    cout << "Coordinates of p1+p2 are : " << (p1.x + p2.x) << ", " << (p1.y + p2.y) << endl;
+    cin >> p2.x >> p2.y >> p2.z;
+    cout << "Coordinates of p1+p2 are : " << (p1.x + p2.x) << ", " << (p1.y + p2.y) << ", " << (p1.z + p2.z) << endl;
 
     return 0;
 }
